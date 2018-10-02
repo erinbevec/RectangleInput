7dd10b184ee63c7386b84d583bbf3c982863d1b7
erinbevec <42977057+erinbevec@users.noreply.github.com>
2018-10-02 13:37:56 -0400

    Add files via upload

---
+++ src/RectangleInput.java
@@ -0,0 +1,17 @@
+/**
+ * Erin Bevec
+ * Rectangle Input
+ * Period 6 Comp Sci
+ */
+import java.util.Scanner;
+public class RectangleInput 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.printf("%",lengthf);
+
+	}
+
+}
+
