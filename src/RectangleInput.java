ad79efe6c14ce3b0a6faf2203cf8928923e88032
erinbevec <42977057+erinbevec@users.noreply.github.com>
2018-10-03 08:34:14 -0400

    Add files via upload

--- src/RectangleInput.java
+++ src/RectangleInput.java
@@ -9,8 +9,26 @@ public class RectangleInput
 
 	public static void main(String[] args)
 	{
-		System.out.printf("%",lengthf);
-
+		Scanner in = new Scanner(System.in);
+		System.out.print("Length of rectange: ");
+		double length = in.nextDouble();
+		in.nextLine();
+		System.out.print("Width of rectange: ");
+		double width = in.nextDouble();
+		in.nextLine();
+		in.close();
+		
+		double area = length * width;
+		double perimeter = 2 * length + 2 * width;
+		double diagonal = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
+		
+		
+		System.out.printf("The area is %6.2f", area);
+		System.out.println();
+		System.out.printf("The perimeter is %6.2f", perimeter);
+		System.out.println();
+		System.out.printf("The length of the diagonal is %6.2f", diagonal);
+		System.out.println();
 	}
 
 }
